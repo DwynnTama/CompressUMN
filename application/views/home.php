3498b4ef19cda5bf5a60594adcc29f57edc80aa5
jleopold88 <jleopold88@yahoo.com>
2020-12-24 22:10:54 +0700

    Carousell

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,6 @@
 <?= $nav ?>
-<div class="container" id="cogan">
+<title>Compress UMN</title>
+<div class="container">
     <p class="title">
         COMPRESS UMN</br>
         <span class="sub-title">Under Construction<span>
@@ -26,16 +27,74 @@
             <img src="https://wallpapercave.com/wp/wp2841697.jpg" class="compress-card-img"/>
         </div>
     </div>  
+    <div class="slider-small">
+        <div class="slides-small">
+                <input type="radio" name ="radio-btn" id="radio1">
+                <input type="radio" name ="radio-btn" id="radio2">
+                <input type="radio" name ="radio-btn" id="radio3">
 
-    <div>
-        ini carrousel
-        <a href="#cogan">COGAN</a>
-    </div>
+                <div class="slide-small first">
+                    <img src="https://wallpaperaccess.com/full/1313700.jpg" alt="">
+                    <div class="text"> <p>Caption Pic 1</p></div>
+                </div>
+
+                <div class="slide-small">
+                    <img src="https://lh3.googleusercontent.com/proxy/xLbg-NqNmPZoBckQkzmLFg0WFGV2O6zgU71g2fpsjLYWiNYb9o8twh1zbvO7b4nEgYPLa2R4x8rtsXSlasiDTKTT39j6JbM" alt="">
+                    <p class="text">Caption Pic 2</p>
+                </div>
+
+                <div class="slide-small">
+                    <img src="https://wonderfulengineering.com/wp-content/uploads/2016/01/cool-wallpaper-4.jpg" alt="">
+                    <p class="text">Caption Pic 3</p>
+                </div>
+
+                <div class="navigation-auto-small">
+                    <div class="auto-btn1"></div>
+                    <div class="auto-btn2"></div>
+                    <div class="auto-btn3"></div>
+                </div>
+        </div>
+            <div class="navigation-manual-small">
+                        <label for="radio1" class="manual-btn"></label>
+                        <label for="radio2" class="manual-btn"></label>
+                        <label for="radio3" class="manual-btn"></label>
+            </div>
+        </div>
 
     <div class="compress-section">
-    <b></b>
+        <b></b>
         <h1>CHECK US OUT !</h1>
         <b></b>
     </div>
+    <div class="slider">
+        <div class="slides">
+                <input type="radio" name ="radio-btn" id="radio1">
+                <input type="radio" name ="radio-btn" id="radio2">
+                <input type="radio" name ="radio-btn" id="radio3">
+
+                <div class="slide first">
+                    <img src="https://wallpaperaccess.com/full/1313700.jpg" alt="">
+                </div>
+
+                <div class="slide">
+                    <img src="https://lh3.googleusercontent.com/proxy/xLbg-NqNmPZoBckQkzmLFg0WFGV2O6zgU71g2fpsjLYWiNYb9o8twh1zbvO7b4nEgYPLa2R4x8rtsXSlasiDTKTT39j6JbM" alt="">
+                </div>
+
+                <div class="slide">
+                    <img src="https://wonderfulengineering.com/wp-content/uploads/2016/01/cool-wallpaper-4.jpg" alt="">
+                </div>
+
+                <div class="navigation-auto">
+                    <div class="auto-btn1"></div>
+                    <div class="auto-btn2"></div>
+                    <div class="auto-btn3"></div>
+                </div>
+        </div>
+            <div class="navigation-manual">
+                        <label for="radio1" class="manual-btn"></label>
+                        <label for="radio2" class="manual-btn"></label>
+                        <label for="radio3" class="manual-btn"></label>
+            </div>
+        </div>
 </div>
 <?= $footer ?>
--- application/views/templates/compress-navbar.php
+++ application/views/templates/compress-navbar.php
@@ -1,6 +1,7 @@
 <html>
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/carousel.css'); ?>">
     <span class="navbar">
         <a href="<?= base_url(); ?>">Home</a>
         <a href="<?= base_url('Oprec'); ?>">Oprec</a>

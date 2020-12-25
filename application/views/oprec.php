bbf428f1445b48f16d93560215d594e0af971421
jleopold88 <jleopold88@yahoo.com>
2020-12-25 17:01:23 +0700

    Oprec Search bar

--- application/views/oprec.php
+++ application/views/oprec.php
@@ -1,4 +1,14 @@
-<link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
-<div class="container">
-    Ini cina</br>
+<title>Open Recruitment</title>
+<div class="box">
+    <h2>Hasil Interview Open Recruitment</h2>
+    <form action="">
+        <div>
+            <input type="text" placeholder="Type...">
+            <select name="" id="">
+                <option>NIM</option>
+                <option>Nama</option>
+            </select>
+        </div>
+        <input type="submit" value="Search">
+    </form>
 </div>
--- application/views/templates/compress-navbar.php
+++ application/views/templates/compress-navbar.php
@@ -2,6 +2,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <link rel="icon" href="<?= base_url('assets/images/compress-logo.png'); ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/oprec.css'); ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/carousel.css'); ?>">
     <span class="navbar">
         <img src="<?= base_url('assets/images/compress.png'); ?>" style="width:100px"/>

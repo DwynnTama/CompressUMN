1f0bc1414b4a61290d64e1a9afe1df5f0353ff59
olwennnn <j.olwennnn@users.noreply.github.com>
2020-12-29 17:20:26 +0700

    iseng push

--- application/views/oprec.php
+++ application/views/oprec.php
@@ -2,10 +2,10 @@
 <title>Open Recruitment</title>
 <div class="container oprec">
     <h2>Open Recruitment</h2>
-    <h1>Cek namamu di sini!</h1>
+    <h5>Cek namamu di sini!</h5>
     <form method="POST" action="<?= base_url('Oprec/Result') ?>" class="compress-form" id="form1">
         <div>
-            <input maxlength="6" name="nama" type="text" placeholder="Masukkan NIM..." id="num" oninput="return onlynum()">
+            <input maxlength="5" name="nama" type="text" placeholder="Masukkan NIM..." id="num" oninput="return onlynum()">
         </div>
         <button type="submit" value="submit" class="compress-btn">SUBMIT</button>
     </form>
@@ -18,7 +18,7 @@
         <?php
         echo '<h3 class="text-nama">' . $row['Nama'] . '</h3>';
         echo '<h3 class="text-NIM">' . $row['NIM'] . '</h3>';
-        echo '<h3>' . $row['Informasi'] . ' </h3><p>ke tahap interview</p>';
+        echo '<p class="text-lower"><strong>' . $row['Informasi'] . '</strong> ke tahap interview</p>';
     } ?>
         </div>
 </div>

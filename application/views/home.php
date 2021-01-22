94531b5db02a2e1b0f8a7cc8e77985da3f60815f
jleopold88 <jleopold88@yahoo.com>
2021-01-22 18:28:34 +0700

    home.php di edit, loading.js di delete

--- application/views/home.php
+++ application/views/home.php
@@ -1,17 +1,20 @@
 <?= $nav ?>
     <script src="<?= base_url("assets/js/slide.js") ?>"></script>
-    <script src="<?= base_url("assets/js/loading.js") ?>"></script>
 <title>COMMPRESS UMN</title>
 <div class="loader">
     <img src="<?= base_url("assets/images/loader.gif") ?>" alt="">
 </div>
-<div class="container">
+<div class="container" id="bg">
     <div class="container-bg"></div>
     <div class="title">
         COMMPRESS UMN</br>
         <span class="sub-title">Coming Soon</span>
     </div>
 </div>
+<script>
+    const loader = document.querySelector('.loader');
+    loader.className += ' hidden';
+</script>
 <div class="content-container" id="section1">
     <span class="instruction-title">
         SCROLL BELOW
--- assets/js/loading.js
+++
@@ -1,4 +0,0 @@
-window.onload = function(){
-    const loader = document.querySelector('.loader');
-    loader.className += ' hidden';
-};

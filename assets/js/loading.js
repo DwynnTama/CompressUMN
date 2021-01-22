7894b953bc4128d31a730e654e48fdd54b840bb7
jleopold88 <jleopold88@yahoo.com>
2021-01-22 17:53:21 +0700

    Update loading.js

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function(){
+window.addEventListener('load', (event) =>{
     const loader = document.querySelector(".loader");
     loader.className += " hidden";
 })

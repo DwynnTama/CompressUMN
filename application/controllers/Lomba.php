4ec83dfe1aed8672c88dcd9fc4dfb88a7898382b
jleopold88 <jleopold88@yahoo.com>
2021-02-24 23:25:18 +0700

    Revert "rombak front end lomba"
    This reverts commit 5daac048a2638ac07efe19ee2f18d095bcbc9bbd.

--- application/controllers/Lomba.php
+++ application/controllers/Lomba.php
@@ -3,21 +3,21 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Lomba extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->library('form_validation');
-    }
+        public function __construct()
+        {
+                parent::__construct();
+                $this->load->library('form_validation');
+        }
 
-    public function index()
-    {
-        $this->load->view('templates/commpress-navbar');
-        $this->load->view('select_lomba');
-        $this->load->view('templates/commpress-footer');
-    }
+        public function index()
+        {
+                $this->load->view('templates/commpress-navbar');
+                $this->load->view('lomba');
+                $this->load->view('templates/commpress-footer');
+        }
 
-    public function registration_feature()
-    {
+        public function registration()
+        {
         $this->form_validation->set_rules('nama', 'Nama', 'required|trim', [
             'required' => "<p class='text-danger'>Username juga tidak boleh kosong!</p>"
         ]);
@@ -34,7 +34,7 @@ class Lomba extends CI_Controller
 
         if ($this->form_validation->run() == false) {
             $this->load->view('templates/commpress-navbar');
-            $this->load->view('feature');
+            $this->load->view('lomba');
             $this->load->view('templates/commpress-footer');
         } else {
             $data = [
@@ -45,52 +45,7 @@ class Lomba extends CI_Controller
             ];
 
             $this->db->insert('list_lomba', $data);
-            header('Location: '.base_url()."Lomba");
+            redirect('lomba');
         }
-    }
-
-    public function registration_podcast()
-    {
-        $this->form_validation->set_rules('nama', 'Nama', 'required|trim', [
-            'required' => "<p class='text-danger'>Username juga tidak boleh kosong!</p>"
-        ]);
-
-        $this->form_validation->set_rules('nim', 'NIM', 'required|trim|integer|is_unique[list_lomba.NIM]', [
-            'required' => "<p>NIM tidak boleh kosong</p>",
-            'is_unique' => "<p>NIM yang dimasukkan sudah terdaftar!</p>"
-        ]);
-
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[list_lomba.Email]', [
-            'required' => "<p>Email tidak boleh kosong!</p>",
-            'is_unique' => "<p>Email yang dimasukkan sudah terdaftar!</p>"
-        ]);
-
-        if ($this->form_validation->run() == false) {
-            $this->load->view('templates/commpress-navbar');
-            $this->load->view('podcast');
-            $this->load->view('templates/commpress-footer');
-        } else {
-            $data = [
-                'nama'  => htmlspecialchars($this->input->post('nama', true)),
-                'nim'   => $this->input->post('nim'),
-                'email' => htmlspecialchars($this->input->post('email', true)),
-                'date' => date("Y-m-d",time())
-            ];
-
-            $this->db->insert('list_lomba', $data);
-            header('Location: '.base_url().'Lomba');
-        }
-    }
-
-    public function podcast(){
-        $this->load->view('templates/commpress-navbar');
-        $this->load->view('podcast');
-        $this->load->view('templates/commpress-footer');
-    }
-
-    public function feature(){
-        $this->load->view('templates/commpress-navbar');
-        $this->load->view('feature');
-        $this->load->view('templates/commpress-footer');
     }
 }
